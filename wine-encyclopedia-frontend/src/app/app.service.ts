d0777ff410181254c42aeb2caaced1f10a5d9ca0
Davide Maria Carmelo Scilletta <davide.scilletta@dedalus.eu>
2025-01-23 12:09:46 +0100

    Update: refactoring variabile cablata con file environment

--- wine-encyclopedia-frontend/src/app/app.service.ts
+++ wine-encyclopedia-frontend/src/app/app.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AppService {
-
+	private rootURL = environment.apiUrl;
 	constructor(private http: HttpClient) { }
-
-	rootURL = '/api';
 
 	getWines(uid: string) {
 		return this.http.post(this.rootURL + '/wines', { uid });
