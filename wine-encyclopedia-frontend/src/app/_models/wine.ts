a218c2468be66460c3d8cb33e93d90e41e10d692
Davide Maria Carmelo Scilletta <davide.scilletta@dedalus.eu>
2025-02-11 17:51:22 +0100

    feat: Aggiunti models wine, winery

---
+++ wine-encyclopedia-frontend/src/app/_models/wine.ts
@@ -0,0 +1,20 @@
+﻿export class Wine {
+    id!: number;
+    wineryName!: string;
+    grapes!: Grapes[];
+    soil!: string;
+    tastingDescription!: string;
+    denomination!: string;
+	productionZone!: string;
+    refinement!: string;
+    name!: string;
+    color!: string;
+    winery!: string;
+    winemaking!: string;
+    img?: string;
+}
+
+export class Grapes {
+    nameGrape!: string;
+    percentageGrape!: number;
+}
---
+++ wine-encyclopedia-frontend/src/app/_models/winery.ts
@@ -0,0 +1,14 @@
+﻿import { Wine } from "./wine";
+
+export class Winery {
+    id?: string;
+    name!: string;
+    city!: string;
+    country!: string;
+    denomination!: string;
+    district!: string;
+    region!: string;
+    village!: string;
+    wines?: Wine[];
+    link?: string;
+}
